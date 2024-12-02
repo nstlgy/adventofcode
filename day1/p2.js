3b39af9b27396f1ec36439d91f764ed335b511ca
nstlgy <yvsh7.btw@gmail.com>
2024-12-02 16:40:43 +0530

    Day1 Part-2 Solution

--- day1/p2.js
+++ day1/p2.js
@@ -1,14 +1,17 @@
 const fs = require("fs");
 
 const input = fs.readFileSync("input.txt", "utf8");
-const test = input.split("\n").map((value) => value.split("  ").map(Number));
+const list = input.split("\n").map((value) => value.split("  ").map(Number));
 
-const leftList = [...test.map((value) => value[0])].sort((a, b) => a - b);
-const rightList = [...test.map((value) => value[1])].sort((a, b) => a - b);
+const leftList = [...list.map((value) => value[0])].sort((a, b) => a - b);
+const rightList = [...list.map((value) => value[1])].sort((a, b) => a - b);
 
-const distance = leftList.map((value, index) => {
-  return Math.abs(value - rightList[index]);
-});
-
-const totalDistance = distance.reduce((sum, curr) => sum + curr, 0);
-console.log(totalDistance);
+let result = 0;
+for (let i = 0; i < leftList.length; i++) {
+  let freq = 0;
+  for (let j = 0; j < rightList.length; j++) {
+    if (leftList[i] === rightList[j]) freq++;
+  }
+  result += leftList[i] * freq;
+}
+console.log(result);
